49254c8421121b55787d7fb3c775ebecac632b84
Jerry <jerrydwalters@gmail.com>
2017-01-18 10:38:14 -0500

    - add datepicker - style filter - tweak styles for individual log

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(function() {
     console.log( "ready!" );
-    
+    $( ".datepicker" ).datepicker();
+    var object = {
+      obj: 'object',
+      boolean: true,
+      seven: 7
+    }
+    var json = JSON.stringify(object, null, 2);
+    console.log($('.tab__content').text())
+    console.log('json:', json)
+    $('.tab__content').empty().append(json);
 });
