97902302d2ab195a3c46ca2ff1740b085c5f6436
Jerry <jerrydwalters@gmail.com>
2017-01-23 08:52:24 -0500

    styled filter options and toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(function() {
     console.log( "ready!" );
+
+    // toggle Filters
+    $('.filter__toggle').click(function() {
+      $('.filter-content').toggle();
+    })
+
+    $('.filter-options__single').click(function() {
+      $(this).toggleClass('filter-options__single--selected');
+    })
+
+    // datepicker
     $( ".datepicker" ).datepicker();
     var object = {
       obj: 'object',
       boolean: true,
       seven: 7
     }
+
+    // turn some json into a string -- need to prettify
     var json = JSON.stringify(object, null, 2);
     console.log($('.tab__content').text())
     console.log('json:', json)
